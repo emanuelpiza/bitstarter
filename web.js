a3d4bef17c9757da74e8faa89e1ea5a656a2dbde
Emanuel Piza <ethales@gmail.com>
2013-07-11 10:42:09 +0000

    passei tudo como funcao

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 
-var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
-var teste = buf.toString();
+//var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
+//var teste = buf.toString();
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(teste);
+  response.send(Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8').toString());
 });
 
 var port = process.env.PORT || 5000;

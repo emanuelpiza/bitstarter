1fc16a76f2f3be211ede5292ec8304d5c046c07e
Emanuel Piza <ethales@gmail.com>
2013-07-11 01:42:55 +0000

    aspas

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var app = express.createServer(express.logger());
 var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString);
+  response.send("buf.toString");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
 });

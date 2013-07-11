23250647b62d5c79287f58b31be0317645f4eee1
Emanuel Piza <ethales@gmail.com>
2013-07-11 01:01:33 +0000

    Ajustado home

--- web.js
+++ web.js
@@ -3,12 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/bitstarter/index.html' 
-	function(err,data){if(err) 
-	    throw err; 
-	    console.log(data)
-	});
-});
+  response.send(fs.readFileSync('~/bitstarter/index.html', function(err,data){if(err)throw err;console.log(data)});});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

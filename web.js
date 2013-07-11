640de8fe9b0b55a4b1d047c32e5817507de30785
Emanuel Piza <ethales@gmail.com>
2013-07-11 10:32:47 +0000

    com variavel teste2

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
-var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
-var teste = buf.toString();
+//var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
+//var teste = buf.toString();
+var teste2 = 'lalala';
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {

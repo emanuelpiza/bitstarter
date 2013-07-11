e4f7fb8de0220b2b2c61c26b7e69b419363958c1
Emanuel Piza <ethales@gmail.com>
2013-07-11 01:31:49 +0000

    ponto e virgula

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send(buf.toString)};
+  response.send(buf.toString);
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

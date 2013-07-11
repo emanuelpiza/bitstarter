6b5fef1dee061a2731aed98f9c04d40dea41b9e4
Emanuel Piza <ethales@gmail.com>
2013-07-11 01:29:24 +0000

    Fiz funcionar no js interno do emacs tanto o buffer quanto o filereadSync.

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), "utf-8");
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('~/bitstarter/index.html', function(err,data){if(err)throw err;console.log(data)});});
+  response.send(buf.toString)};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

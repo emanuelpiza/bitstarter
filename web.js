bfd6efeb9587b0ea4166cab35f86885f4318621b
Emanuel Piza <ethales@gmail.com>
2013-07-12 00:48:55 +0000

    passano a variavel

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var buf = new Buffer(fs.readFileSync('./index.html'), 'utf-8');
     var teste = buf.toString();
-    response.send("Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8').toString()");
+    response.send(teste);
 });
 
 var port = process.env.PORT || 5000;

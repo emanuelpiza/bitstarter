f8fae98109112e29c3d4dce0fcaec98579245cdd
Emanuel Piza <ethales@gmail.com>
2013-07-12 00:42:20 +0000

    usando internamente

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
-
-//var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
-//var teste = buf.toString();
-
 var express = require('express');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send("Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8').toString()");
+    var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8');
+    var teste = buf.toString();
+    response.send("Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'), 'utf-8').toString()");
 });
 
 var port = process.env.PORT || 5000;
